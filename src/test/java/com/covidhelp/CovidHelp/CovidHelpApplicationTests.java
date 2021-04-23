3475ef432306b89d5f7e062f5f563437949e51bd
Atul Yadav <atuy@amazon.com>
2021-04-23 11:12:26 +0530

    Let's do this.

---
+++ src/test/java/com/covidhelp/CovidHelp/CovidHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covidhelp.CovidHelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

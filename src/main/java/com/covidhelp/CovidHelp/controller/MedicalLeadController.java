90f7c20e95a9184a4ee7ae5d120d0057b0f78a03
Atul Yadav <atuy@amazon.com>
2021-04-24 16:17:19 +0530

    Add API to fetch a medical lead by id

--- src/main/java/com/covidhelp/CovidHelp/controller/MedicalLeadController.java
+++ src/main/java/com/covidhelp/CovidHelp/controller/MedicalLeadController.java
@@ -5,6 +5,7 @@ import com.covidhelp.CovidHelp.api.MedicalLeadResponse;
 import com.covidhelp.CovidHelp.data.MedicalLead;
 import com.covidhelp.CovidHelp.service.MedicalLeadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,20 @@ public class MedicalLeadController {
 
     @Autowired
     private MedicalLeadService medicalLeadService;
+
+    @RequestMapping(value = "/{medicalLeadId}", method = RequestMethod.GET)
+    public MedicalLeadResponse getMedicalLeads(@PathVariable(value = "medicalLeadId") String medicalLeadId) {
+        MedicalLead medicalLead = medicalLeadService.getMedicalLead(medicalLeadId);
+        return MedicalLeadResponse.builder()
+                .id(medicalLead.getId())
+                .mobiles(medicalLead.getMobiles())
+                .name(medicalLead.getName())
+                .telegramLink(medicalLead.getTelegramLink())
+                .medicalLeadType(medicalLead.getMedicalLeadType())
+                .verifiedCount(medicalLead.getVerifiedCount())
+                .rejectCount(medicalLead.getRejectCount())
+                .build();
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<MedicalLeadResponse> getMedicalLeads() {
--- src/main/java/com/covidhelp/CovidHelp/service/MedicalLeadService.java
+++ src/main/java/com/covidhelp/CovidHelp/service/MedicalLeadService.java
@@ -7,12 +7,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MedicalLeadService {
 
     @Autowired
     private MedicalLeadRepository medicalLeadRepository;
+
+    public MedicalLead getMedicalLead(String medicalLeadId) {
+        Optional<MedicalLead> medicalLeadOptional = medicalLeadRepository.findById(medicalLeadId);
+        if(medicalLeadOptional.isPresent()) {
+            return medicalLeadOptional.get();
+        } else {
+            throw new RuntimeException("no medical lead found with given id");
+        }
+    }
 
     public List<MedicalLead> getMedicalLeads() {
         return medicalLeadRepository.findAll();
